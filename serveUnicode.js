c7768116307b1e293801c159433a01b2601ce3f1
Karl <tyson.karl@gmail.com>
2015-09-27 17:17:34 -0500

    changes to get set up on CC

---
+++ serveUnicode.js
@@ -0,0 +1,74 @@
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.argv[2] || 8080;
+
+var pts = require ("./makegrid.js");
+
+
+http.createServer(function(request, response) {
+
+  
+var uri = url.parse(request.url).pathname, 
+    filename = path.join(process.cwd(), uri),
+    pathkey = uri.substr(uri.lastIndexOf('/') + 1);
+
+console.log("ok", uri, filename, pathkey);
+
+      if (pathkey == "make") {
+        var incoming = '';
+
+        request.on('data', function(data) {
+
+          incoming += data;
+console.log(" we got: " + incoming);
+        
+        });
+
+        request.on("end", function() {
+
+           if(incoming) {
+
+			  	var ptsArr = pts.makeGrid();
+				
+              	response.end(JSON.stringify(ptsArr));           
+            
+          } 
+                
+        });
+
+
+    } else {
+
+
+      //STATIC FILES
+   
+	  if (uri === '/favicon.ico') {
+		response.writeHead(200, {'Content-Type': 'image/x-icon'} );
+		response.end();
+		//console.log('favicon requested');
+		return;
+	  }
+	  	  
+      if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+      fs.readFile(filename, "binary", function(err, file) {
+        if(err) {        
+          response.writeHead(500, {"Content-Type": "text/plain"});
+          response.write(err + "\n");
+          response.end();
+          return;
+        }
+
+        response.writeHead(200);
+        response.write(file, "binary");
+        response.end();
+      });
+
+
+    }
+
+}).listen(parseInt(port, 10));
+
+console.log("Server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
